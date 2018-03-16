07118cc7d6337e68a4f692ec843e8192551beaed
wrogoz <wrogoz@gmail.com>
2018-03-16 22:25:44 +0100

    dodano: - eventListener na kw. - dodano warunki sprawdzające poprawność wyboru - brak losowego wywbierania kolorow

--- colorGame.js
+++ colorGame.js
@@ -10,14 +10,44 @@ var colors=[
 
 var squares = document.querySelectorAll(".square");
 
+// stworzenie zmiennej do odgadnięcia
+
 var pickedColor=colors[3];
+
+//zaznaczenie zmiennej(rgb) w tagu h1
+
 var colorDisplay=document.querySelector("#colorDisplay");
-colorDisplay.textContent=colors[3];
+
+// wyswietlenie wartosci zmiennej do odgadnięcia 
+
+colorDisplay.textContent=pickedColor;
 
 
 for(var i=0; i<squares.length; i++){
 
+// nalozenie kolorow na kazdy z kwadratów
+
 squares[i].style.backgroundColor=colors[i];
 
+// dodanie clickListener na kwadraty
+
+squares[i].addEventListener("click", function(){
+
+// pobranie koloru z kliknietego kwadratu
+
+var clickedColor=this.style.backgroundColor;
+
+// porównanie tego koloru z docelowym 
+
+if(clickedColor===pickedColor){
+alert("yes");
+}
+else{
+	alert("no");
+}
+
+
+
+});
 
 }
